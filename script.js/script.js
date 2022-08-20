bf0a46dc73bed400d3795335f036efbe42e733e5
Maria Belen Lico <mariabelen@MacBook-Pro-de-Maria-2.local>
2022-08-20 19:58:00 -0300

    inicio decommit

---
+++ script.js/script.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector('#menu');
+
+
+let menuBar = document.querySelector('#menuBar');
+
+menuBar.addEventListener('click', function() {
+    menu.classList.toggle("menu-toggle");
+    
+  });
+
